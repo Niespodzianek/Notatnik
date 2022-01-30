12b0127fb4c0ecb8c6175170a9f08a7be229961e
Niespodzianek <64963974+Niespodzianek@users.noreply.github.com>
2022-01-30 22:07:06 +0100

    Rozwój do wersji 2
    Dopisanie kodu z podpunktów todo.

--- main 2.py
+++ main 2.py
@@ -1,67 +1,50 @@
 import os
-
-
 # Funkcja konteks menadżera odczytu notatki
 def odczyt_z_pliku(sciezka, flaga="r"):
     with open(sciezka, mode=flaga) as odczytany_plik:
         dane_odczytane_z_pliku = odczytany_plik.read()
         print(dane_odczytane_z_pliku)
-
-
 # Funkcja konteks menadżera zapisu notatki
 def zapis_do_pliku(sciezka, tekst_do_zapisu="", flaga="w"):
     with open(sciezka, mode=flaga) as plik_do_zapisu:
         plik_do_zapisu.write(tekst_do_zapisu)
-
-
 # W tej funkcji wpisujemy tekst, który zostanie zapisany w nowej notatce lub dopisany na końcu notatki.
-def tekst_notatki():
+def generator_nowego_tekstu_notatki():
     wpis = input("Wpisz tekst, który ma być zapisany w notatce:\n")
     return wpis
-
-
+# W tej funkcji wpisujemy nazwę pliku notatki.
+def generator_nazwy_pliku_notatki():
+    nazwa_notatki = input("Wpisz nazwę nowej notatki bez rozszerzenia czyli nazwę nowego pliku, w którym będzie zapisana notatka:\n")
+    nazwa_pliku_notatki = nazwa_notatki + ".txt"
+    return nazwa_pliku_notatki
 # Funkcja odczytująca zawartość pliku lub zapisująca tekst do pliku
 def operacja_na_pliku(typ_dzialania):
-	lista_notatek = []
-	sciezka_do_pliku = os.path.join("../Dane", "tekst.txt")
-	sciezka_do_katalogu = os.path.join("../Dane")
-    try:
-        if typ_dzialania == "odczyt":
-        	#lista_notatek = os.listdir(sciezka_do_katalogu)
-            odczyt_z_pliku(sciezka=sciezka_do_pliku)
-        elif typ_dzialania == "nowa":
-            zapis_do_pliku(
-                sciezka=sciezka_do_pliku, tekst_do_zapisu=tekst_notatki()
-            )
-        elif typ_dzialania == "dopisek":
-            dopis = tekst_notatki()
-            zapis_do_pliku(
-                sciezka=sciezka_do_pliku,
-                tekst_do_zapisu=(f"\n" + f"{dopis}"),
-                flaga="a",
-            )
-        elif typ_dzialania == "kasowanie":
-            zapis_do_pliku(sciezka=sciezka_do_pliku)
-
-    except IOError as error:
-        print(
-            f"Nie udało się odczytać pliku, błąd typu - {type(error)} - {error}"
-        )
-    else:
-        print("Wszystko poszło prawidłowo")
-    finally:
-        input("Koniec fazy pracy z plikiem, naciśnij ENTER i wracamy do MENU")
-
-
+	sciezka_do_pliku = os.path.join("Dane", "tekst3.txt")
+	try:
+	    if typ_dzialania == "odczyt":
+	        lista_notatek = os.listdir(os.path.join("Dane"))
+	        print(lista_notatek)
+	    elif typ_dzialania == "nowa":
+	        sciezka_do_pliku = os.path.join("Dane", generator_nazwy_pliku_notatki())
+	        zapis_do_pliku(sciezka=sciezka_do_pliku, tekst_do_zapisu=generator_nowego_tekstu_notatki())
+	    elif typ_dzialania == "dopisek":
+	        dopis = generator_nowego_tekstu_notatki()
+	        zapis_do_pliku(sciezka=sciezka_do_pliku, tekst_do_zapisu=(f"\n" + f"{dopis}"),flaga="a")
+	    elif typ_dzialania == "kasowanie":
+	        zapis_do_pliku(sciezka=sciezka_do_pliku)
+	except IOError as error:
+	    print(f"Nie udało się dokonać operacji na pliku, błąd typu - {type(error)} - {error}")
+	else:
+	    print("Wszystko poszło prawidłowo")
+	finally:
+	    input("Koniec fazy pracy z plikiem, naciśnij ENTER i wracamy do MENU")
 # Główne menu programy to w nim dokonujemy wyboru operacji na notatce
 def program_notatnik():
     while True:
         os.system("clear")
         print(20 * "*" + " MENU PROGRAMU NOTATNIK " + 20 * "*")
         wybor_menu = input(
-            "(1)Notatki\n(2)Nowa notatka\n(3)Dopisek do notatki\n(4)Kasowanie\n(5)Koniec pracy"
-            " programu\nTwój wybór: "
-        )
+            "(1)Notatki\n(2)Nowa notatka\n(3)Dopisek do notatki\n(4)Kasowanie\n(5)Koniec pracy programu\nTwój wybór: ")
         if wybor_menu == "1":
             print("Odczyt notatki")
             operacja_na_pliku(typ_dzialania="odczyt")
@@ -75,20 +58,15 @@ def program_notatnik():
             print("Kasowanie notatki")
             operacja_na_pliku(typ_dzialania="kasowanie")
         elif wybor_menu == "5":
-            input(
-                "Koniec pracy programu. Naciśnij ENTER aby zakończyć program."
-            )
+            input("Koniec pracy programu. Naciśnij ENTER aby zakończyć program.")
             exit()
         else:
             print("Zły wybór !!! Jeszcze raz.")
         input("Naciśnij ENTER aby kontynuować.")
-
-
 if __name__ == "__main__":
     program_notatnik()
-
-# TODO: Plan
-# TODO: Wersja 5 ChmurA
+# TODO: Plan:
+# TODO: Wersja 5 Chmura
 # TODO: Wersja 4 Środowisko graficzne
 # TODO: Wersja 3 Lista notatek gdzie nazwa pliku notatki ma być nazwą notatki i wszystkie wcześniejsze operacje<
 # TODO: Wersja 2 Wybór notatki, którą się chce czytać, kasować, dopisywać zawartość poprzez jej wybranie i lub wpisanie
--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-sklad = os.listdir()
-print(sklad)
-for index, notatka in enumerate(sklad):
-	print(f"Notatka nr {index + 1} to {notatka}")
